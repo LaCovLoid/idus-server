462a492de623061ac0ee23ab2e7adff30ba84c3d
LaCovLoid <tnsgus0018@naver.com>
2024-03-27 04:18:46 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { json, request, urlencoded } from "express";
+import { createConnection } from "mysql2/promise";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+let connection: any;
+
+dotenv.config();
+connectServer();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+async function connectServer() {
+  connection = await createConnection({
+    host: process.env.DB_HOST, // 'localhost',
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  console.log("connection successful?", connection != null);
+}
+
+app.get("/", randomHandler);
+async function randomHandler(req: any, res: any) {
+  if (connection == null) return;
+
+  let [result] = await connection.query(
+    "SELECT * FROM `words` WHERE `yomi_word_same`='0' Order by rand() Limit 1"
+  );
+  res.send(result);
+}
